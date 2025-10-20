c24704682b8a6697bcb42a51ce7570201ebe43ef
Laitheis <gosha.reshetnikov2000@gmail.com>
2025-10-20 08:22:18 +0400

    fix: some bugs

--- Assets/Game/Scripts/Not Sorted/WorldSelectable.cs
+++ Assets/Game/Scripts/Not Sorted/WorldSelectable.cs
@@ -3,6 +3,15 @@
 [RequireComponent(typeof(Outline))]
 public class WorldSelectable : MonoBehaviour
 {
-
+    private void Reset()
+    {
+        var outline = GetComponent<Outline>();
+        if(outline)
+        {
+            outline.enabled = false;
+        }
+    }
 }
+
+
 
---
+++ Assets/Game/Scripts/WorldObjects/Grass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Grass : DestructibleObjectBase
+{
+    public override void InitializeStats()
+    {
+        base.InitializeStats();
+
+        _statContainer.GetStat(StatTypes.Durability).Value = 10;
+    }
+
+    protected override void InitializeLoot()
+    {
+        base.InitializeLoot();
+
+        _pendingLoot = _lootGenerator.GenerateLoot("Grass", 0);
+    }
+}
+
